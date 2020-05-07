bee250ca749fdbdc184418c82db91802092497e7
WoojinKim <hyung12379@gmail.com>
2020-05-07 18:15:18 +0900

    add uploaded_time

--- crawl.py
+++ crawl.py
@@ -14,7 +14,8 @@ from selenium.webdriver.common.keys import Keys
 def crawl_comment(url):
     comment_data = pd.DataFrame({'content':[],
                                 'subcomment_num':[],
-                                'like_num':[]})
+                                'like_num':[],
+                                'uploaded_time':[]})
     options = Options()
     options.add_argument('--start-maximized')
     
@@ -49,7 +50,8 @@ def crawl_comment(url):
     browser.find_element_by_xpath('//paper-listbox[@class="dropdown-content style-scope yt-dropdown-menu"]/a[1]').click()
     
     # n번 스크롤하기 이거 늘리면 댓글 개수 늘어남!!!
-    num_page_down = 10
+    # n = 원하는 댓글 개수 // 5
+    num_page_down = 200
     while num_page_down:
         body.send_keys(Keys.PAGE_DOWN)
         time.sleep(1.5)
@@ -97,8 +99,34 @@ def crawl_comment(url):
                 like_num = int(like_num)
         except:
             like_num = 0
-        
-        new_comment_data =  pd.DataFrame({'content':[comment], 'subcomment_num':[subcomment_num], 'like_num':[like_num]})
+            
+        try:
+            uploaded_time = comment_block[i].find('yt-formatted-string', {'class':'published-time-text above-comment style-scope ytd-comment-renderer'}).find('a',{'class':'yt-simple-endpoint style-scope yt-formatted-string'}).text
+            print(uploaded_time)
+            uploaded_time = uploaded_time.split(' ')[0]
+            print(uploaded_time)
+            if('분' in uploaded_time or '초' in uploaded_time):
+                uploaded_time = 0
+            elif('시간' in uploaded_time):
+                uploaded_time = uploaded_time.rstrip('시간').strip()
+                uploaded_time = int(uploaded_time)
+            elif('일' in uploaded_time):
+                uploaded_time = uploaded_time.rstrip('일').strip()
+                uploaded_time = int(uploaded_time) * 24
+            elif('주' in uploaded_time):
+                uploaded_time = uploaded_time.rstrip('주').strip()
+                uploaded_time = int(uploaded_time) * 24 * 7
+            elif('개월' in uploaded_time):
+                uploaded_time = uploaded_time.rstrip('개월').strip()
+                print(uploaded_time)
+                uploaded_time = int(uploaded_time) * 24 * 30
+                print(uploaded_time)
+            elif('년' in uploaded_time):
+                uploaded_time = uploaded_time.rstrip('년').strip()
+                uploaded_time = int(uploaded_time) * 24 * 365
+        except:
+            print('error')
+        new_comment_data =  pd.DataFrame({'content':[comment], 'subcomment_num':[subcomment_num], 'like_num':[like_num], 'uploaded_time':[uploaded_time]})
         comment_data = comment_data.append(new_comment_data)
     
     comment_data.index = range(len(comment_data))
